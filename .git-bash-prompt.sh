596bc427e6678c25f270db3d2439eb4ee353d85d
Rodrigo Ramalho Araujo <rodrigoraraujo@gmail.com>
2023-08-07 21:22:39 -0300

    Update .git-bash-prompt.sh

--- .git-bash-prompt.sh
+++ .git-bash-prompt.sh
@@ -1,85 +1,70 @@
 #!bin/bash
+################################# 
+#####  Git Prompt Info ##########
+#################################
 
-############################# 
-#####                   #####
-#####  Git Prompt Info  #####
-#####                   #####
-#############################
+RESET="\033[0m"
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+PURPLE="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
 
-git_find_repo_path ()
-{
-	if [ -n "${__git_repo_path-}" ]; then
-		# we already know where it is
-		return
-	fi
-
-	if [ -n "${__git_C_args-}" ]; then
-		__git_repo_path="$(git "${__git_C_args[@]}" \
-			${__git_dir:+--git-dir="$__git_dir"} \
-			rev-parse --absolute-git-dir 2>/dev/null)"
-	elif [ -n "${__git_dir-}" ]; then
-		test -d "$__git_dir" &&
-		__git_repo_path="$__git_dir"
-	elif [ -n "${GIT_DIR-}" ]; then
-		test -d "$GIT_DIR" &&
-		__git_repo_path="$GIT_DIR"
-	elif [ -d .git ]; then
-		__git_repo_path=.git
-	else
-		__git_repo_path="$(git rev-parse --git-dir 2>/dev/null)"
-	fi
-}
-
-git_find_repo_path
+BRIGHT_BLACK="\033[1;30m"
+BRIGHT_RED="\033[1;31m"
+BRIGHT_GREEN="\033[1;32m"
+BRIGHT_YELLOW="\033[1;33m"
+BRIGHT_BLUE="\033[1;34m"
+BRIGHT_PURPLE="\033[1;35m"
+BRIGHT_CYAN="\033[1;36m"
+BRIGHT_WHITE="\033[1;37m"
 
 git_branch() {   
 
     if [ -d $__git_repo_path ]; then	
            branch=$(git symbolic-ref --short HEAD 2>/dev/null)
         if [ $? -eq 0 ]; then
-           echo " ($branch)"
+           echo -e " ${BRIGHT_YELLOW}($branch)${RESET}"
         fi
     fi	
 }
 
-git_prompt_new() {
-     if [ -d $__git_repo_path ]; then	
-        new=$(git status --porcelain 2>/dev/null | grep '^??' | wc -l)
+is_git_repo() {
+    git rev-parse --is-inside-work-tree &>/dev/null
+}
+
+git_prompt() {
+    if is_git_repo; then
+        # git_status="$(git status --porcelain 2> /dev/null)"
+        git_status=$(git status --porcelain 2>/dev/null)        
+
+        local new=$(printf "%s\n" "$git_status"         | grep '^??' | wc -l)
+        local modified=$(printf "%s\n" "$git_status"    | grep '^ M'    | wc -l) 
+        local deleted=$(printf "%s\n" "$git_status"     | grep '^ D'    | wc -l)
+        local staged=$(printf "%s\n" "$git_status"      | grep '^[AMR]' | wc -l)
+        local prompt=''
         
         if [ "$new" -gt 0 ]; then
-            echo " +$new"
+            prompt="${BLUE}+$new "
         fi
-    fi
-}
 
-git_prompt_modified() {
-     if [ -d $__git_repo_path ]; then	
-        modified=$(git status --porcelain 2>/dev/null | grep '^ M' | wc -l) 
-        
         if [ "$modified" -gt 0 ]; then
-            echo " *$modified"
+            prompt+="${YELLOW}*$modified "
         fi
-    fi
-}
-
-git_prompt_deleted() {
-     if [ -d $__git_repo_path ]; then	
-        deleted=$(git status --porcelain 2>/dev/null | grep '.*D' | wc -l)
         
         if [ "$deleted" -gt 0 ]; then
-            echo " -$deleted"
+            prompt+="${RED}-$deleted "
         fi
-    fi
-}
 
-git_prompt_staged() {
-     if [ -d $__git_repo_path ]; then	
-        staged=$(git status --porcelain 2>/dev/null | grep '^[AMR]' | wc -l)
-        
         if [ "$staged" -gt 0 ]; then
-            echo " ~$staged"
+            prompt+="${GREEN}~$staged "
         fi
+
+        echo -e " $prompt${RESET}"            
     fi
 }
 
-PS1='\[\e[1m\e[32m\]\u@\h\[\e[0m\]:\[\e[1m\e[34m\]\w\[\e[33m\]$(git_branch)\[\e[0m\]\[\e[0m\e[36m\]$(git_prompt_new)\[\e[0m\e[35m\]$(git_prompt_modified)\[\e[0m\e[31m\]$(git_prompt_deleted)\[\e[0m\e[32m\]$(git_prompt_staged)\[\e[0m\]\$ '
+PS1='\[\e[1m\e[32m\]\u@\h\[\e[0m\]:\[\e[1m\e[34m\]\w\[\e[0m\]$(git_branch)$(git_prompt) $ '
