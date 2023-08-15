af3e1a2cff474d4588355f54fa2579f624fcfa93
Rodrigo Ramalho Araujo <rodrigoraraujo@gmail.com>
2023-08-15 19:20:20 -0300

    Update .git-bash-prompt.sh

--- .git-bash-prompt.sh
+++ .git-bash-prompt.sh
@@ -40,7 +40,7 @@ git_prompt() {
         local deleted=$(echo $git_status    | grep '^ D'    | wc -l | awk '{ print $1 }')
         local staged=$(echo $git_status     | grep '^[AMR]' | wc -l | awk '{ print $1 }')
         local prompt=''
-
+        
         if [ "$new" -gt 0 ]; then
             prompt="%{$FG[021]%}+$new "
         fi
@@ -48,8 +48,25 @@ git_prompt() {
         if [ "$modified" -gt 0 ]; then
             prompt+="%{$FG[208]%}*$modified "
         fi
-
+        
         if [ "$deleted" -gt 0 ]; then
+            prompt+="%{$FG[196]%}-$deleted "
+        fi
 
-^G Get Help                          ^O WriteOut                          ^R Read File                         ^Y Prev Pg                           ^K Cut Text                          ^C Cur Pos                           
-^X Exit                              ^J Justify                           ^W Where is                          ^V Next Pg                           ^U UnCut Text                        ^T To Spell                          
+        if [ "$staged" -gt 0 ]; then
+            prompt+="%{$FG[046]%}~$staged "
+        fi
+
+        echo "$prompt%{$reset_color%}"            
+    fi
+}
+
+PROMPT="╭─%{$FG[040]%}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} %{$FG[033]%}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} %{$terminfo[bold]$FG[226]%}%~%{$reset_color%}\$(git_prompt_info) \$(git_prompt)\$(ruby_prompt_info)
+╰─\$(virtualenv_info)\$(prompt_char) "
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$FG[239]%}on%{$reset_color%} %{$fg[255]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$FG[202]%}✘"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$FG[040]%}✔"
+ZSH_THEME_RUBY_PROMPT_PREFIX=" %{$FG[239]%}using%{$FG[243]%} ‹"
+ZSH_THEME_RUBY_PROMPT_SUFFIX="›%{$reset_color%}"
