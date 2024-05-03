19df1287e9f67385eb339db7d98f37acee679dda
Rodrigo Ramalho Araujo <rodrigoraraujo@gmail.com>
2024-05-03 18:23:25 -0300

    Update .git-bash-prompt.sh

--- .git-bash-prompt.sh
+++ .git-bash-prompt.sh
@@ -1,30 +1,35 @@
-# fino-time.zsh-theme
+#!bin/bash
+################################# 
+#####  Git Prompt Info ##########
+#################################
 
-# Use with a dark background and 256-color terminal!
-# Meant for people with RVM and git. Tested only on OS X 10.7.
+RESET="\033[0m"
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+PURPLE="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
 
-# You can set your computer name in the ~/.box-name file if you want.
+BRIGHT_BLACK="\033[1;30m"
+BRIGHT_RED="\033[1;31m"
+BRIGHT_GREEN="\033[1;32m"
+BRIGHT_YELLOW="\033[1;33m"
+BRIGHT_BLUE="\033[1;34m"
+BRIGHT_PURPLE="\033[1;35m"
+BRIGHT_CYAN="\033[1;36m"
+BRIGHT_WHITE="\033[1;37m"
 
-# Borrowing shamelessly from these oh-my-zsh themes:
-#   bira
-#   robbyrussell
-#
-# Also borrowing from http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
+git_branch() {   
 
-function virtualenv_info {
-    [ $CONDA_DEFAULT_ENV ] && echo "($CONDA_DEFAULT_ENV) "
-    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
-}
-
-function prompt_char {
-    git branch >/dev/null 2>/dev/null && echo '⠠⠵' && return
-    echo '○'
-}
-
-function box_name {
-  local box="${SHORT_HOST:-$HOST}"
-  [[ -f ~/.box-name ]] && box="$(< ~/.box-name)"
-  echo "${box:gs/%/%%}"
+    if [ -d $__git_repo_path ]; then	
+           branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+        if [ $? -eq 0 ]; then
+           echo -e " ${BRIGHT_YELLOW}($branch)${RESET}"
+        fi
+    fi	
 }
 
 is_git_repo() {
@@ -33,40 +38,33 @@ is_git_repo() {
 
 git_prompt() {
     if is_git_repo; then
-        local git_status=$(git status --porcelain)
+        # git_status="$(git status --porcelain 2> /dev/null)"
+        git_status=$(git status --porcelain 2>/dev/null)        
 
-        local new=$(echo $git_status        | grep '^??'    | wc -l | awk '{ print $1 }')
-        local modified=$(echo $git_status   | grep '^ M'    | wc -l | awk '{ print $1 }')
-        local deleted=$(echo $git_status    | grep '^ D'    | wc -l | awk '{ print $1 }')
-        local staged=$(echo $git_status     | grep '^[AMR]' | wc -l | awk '{ print $1 }')
+        local new=$(printf "%s\n" "$git_status"         | grep '^??' | wc -l)
+        local modified=$(printf "%s\n" "$git_status"    | grep '^ M'    | wc -l) 
+        local deleted=$(printf "%s\n" "$git_status"     | grep '^ D'    | wc -l)
+        local staged=$(printf "%s\n" "$git_status"      | grep '^[AMR]' | wc -l)
         local prompt=''
         
         if [ "$new" -gt 0 ]; then
-            prompt="%{$FG[021]%}+$new "
+            prompt="${BLUE}+$new "
         fi
 
         if [ "$modified" -gt 0 ]; then
-            prompt+="%{$FG[208]%}*$modified "
+            prompt+="${YELLOW}*$modified "
         fi
         
         if [ "$deleted" -gt 0 ]; then
-            prompt+="%{$FG[196]%}-$deleted "
+            prompt+="${RED}-$deleted "
         fi
 
         if [ "$staged" -gt 0 ]; then
-            prompt+="%{$FG[046]%}~$staged "
+            prompt+="${GREEN}~$staged "
         fi
 
-        echo "$prompt%{$reset_color%}"            
+        echo -e " $prompt${RESET}"            
     fi
 }
 
-PROMPT="╭─%{$FG[040]%}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} %{$FG[033]%}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} %{$terminfo[bold]$FG[226]%}%~%{$reset_color%}\$(git_prompt_info) \$(git_prompt)\$(ruby_prompt_info)
-╰─\$(virtualenv_info)\$(prompt_char) "
-
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$FG[239]%}on%{$reset_color%} %{$fg[255]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$FG[202]%}✘"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$FG[040]%}✔"
-ZSH_THEME_RUBY_PROMPT_PREFIX=" %{$FG[239]%}using%{$FG[243]%} ‹"
-ZSH_THEME_RUBY_PROMPT_SUFFIX="›%{$reset_color%}"
+PS1='\[\e[1m\e[32m\]\u@\h\[\e[0m\]:\[\e[1m\e[34m\]\w\[\e[0m\]$(git_branch)$(git_prompt) $ '
